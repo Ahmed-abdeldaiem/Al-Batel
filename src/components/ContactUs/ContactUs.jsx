80d584c931351377b27cbb41065fe7047aeabbcc
Ahmed Mohamed Saad Abdeldaiem <88617071+Ahmed-abdeldaiem@users.noreply.github.com>
2024-11-26 17:48:03 +0200

    deploy3-2

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -303,7 +303,7 @@ export default function ContactUs() {
                 />
                 <label
                   htmlFor="name"
-                  className="peer-focus:font-medium absolute text-lg text-blue-900 dark:text-gray-400 duration-300 transform translate-y-6 scale-75 start-1 top-1 start-1 -z-10  peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  className="peer-focus:font-medium absolute text-lg text-blue-900 dark:text-gray-400 duration-300 transform translate-y-6 scale-75  top-1 start-1 -z-10  peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                    Name
                 </label>
